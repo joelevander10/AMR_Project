421d37b1e03036568c9b4bf74e976d4e9eda2d55
Jo'el Evander Nathanael Irawan <90888938+joelevander10@users.noreply.github.com>
2024-06-19 10:00:22 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 import threading
+import rospy
 from sensors import lidar
 from sensors import imu
 
 def main():
+    rospy.init_node('lidar_imu_listener', anonymous=True)
+    
     lidar_thread = threading.Thread(target=lidar.start_lidar_subscriber)
     imu_thread = threading.Thread(target=imu.imu_data_collection)
     
     lidar_thread.start()
     imu_thread.start()
+    
+    rospy.spin()
     
     lidar_thread.join()
     imu_thread.join()
