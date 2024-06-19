62ddf8175ac5c40a1b43ce1ef5b95045e4f6b17e
Jo'el Evander Nathanael Irawan <90888938+joelevander10@users.noreply.github.com>
2024-06-19 10:19:40 +0700

    Update lidar.py

--- src/sensors/lidar.py
+++ src/sensors/lidar.py
@@ -8,8 +8,12 @@ min_time_threshold = 0.01
 velocity_threshold = 0.23
 moving_average_window = 2
 
-def lidar_callback(data):
-    global previous_centroid, previous_time, velocity_history, data_count, averaged_data_count
+previous_centroid = None
+previous_time = None
+velocity_history = []
+
+def lidar_callback(data, sensor_data):
+    global previous_centroid, previous_time, velocity_history
     
     points = list(pc2.read_points(data, field_names=("x", "y", "z"), skip_nans=True))
     
@@ -22,7 +26,8 @@ def lidar_callback(data):
     
     current_centroid = centroid(points)
     current_time = rospy.Time.now().to_sec()
-    print(f"Centroid: x={current_centroid[0]:.6f}, y={current_centroid[1]:.6f}, z={current_centroid[2]:.6f}")
+    
+    sensor_data.centroid = current_centroid
     
     if previous_centroid is not None:
         dist = distance(current_centroid, previous_centroid)
@@ -38,17 +43,13 @@ def lidar_callback(data):
             
             if len(velocity_history) == moving_average_window:
                 avg_velocity = moving_average(velocity_history)
-                print(f"Velocity using Centroid: {avg_velocity:.6f} m/s")
-                print("---")
+                sensor_data.velocity = avg_velocity
                 velocity_history = []
-                averaged_data_count += 1
         else:
             rospy.logwarn("Time difference too small. Skipping velocity calculation.")
     
     previous_centroid = current_centroid
     previous_time = current_time
-    
-    data_count += 1
 
 def centroid(points):
     x_sum = y_sum = z_sum = 0
@@ -85,19 +86,3 @@ def is_lidar_stationary(points, threshold=0.01):
         max_z = max(max_z, z)
     
     return (max_x - min_x) < threshold and (max_y - min_y) < threshold and (max_z - min_z) < threshold
-
-def start_lidar_subscriber():
-    global previous_centroid, previous_time, velocity_history, data_count, averaged_data_count
-    
-    previous_centroid = None
-    previous_time = None
-    velocity_history = []
-    data_count = 0
-    averaged_data_count = 0
-    
-    rospy.Subscriber('/lslidar_point_cloud', PointCloud2, lidar_callback)
-    
-    rate = rospy.Rate(22)  # Sampling rate of 22 Hz
-    
-    while not rospy.is_shutdown():
-        rate.sleep()
