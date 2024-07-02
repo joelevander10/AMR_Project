fa9f5de669e3c0b2c5738831c56c2cad6b74f142
Jo'el Evander Nathanael Irawan <90888938+joelevander10@users.noreply.github.com>
2024-07-02 14:44:15 +0700

    Create sensor_data.py

---
+++ ros/sensor_data.py
@@ -0,0 +1,12 @@
+class SensorData:
+    def __init__(self):
+        self.acceleration = None
+        self.magnetic = None
+        self.gyroscope = None
+        self.linear_acceleration = None
+        self.gravity = None
+        self.euler = None
+        self.quaternion = None
+        self.relative_orientation = None
+        self.centroid = None
+        self.velocity = None
